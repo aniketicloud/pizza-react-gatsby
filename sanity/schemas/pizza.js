6e401a032a0acf2750e83fe740145e8a550dec8b
Aniket Pandharabale <aniket.pandharabale@yash.com>
2021-04-02 22:32:45 +0530

    data relations between pizza and toppings

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -1,7 +1,7 @@
 import { MdLocalPizza as icon } from 'react-icons/md';
 
 export default {
-  // computer name
+  // Computer Name
   name: 'pizza',
   // visible title
   title: 'Pizzas',
@@ -36,8 +36,35 @@ export default {
       title: 'Price',
       type: 'number',
       description: 'Price of the pizza in cents',
-      validation: (Rule) => Rule.min(1000).max(50000),
-      // TODO: add custom component
+      validation: (Rule) => Rule.min(1000),
+    },
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'topping' }] }],
+      // TODO: Add custom input component
     },
   ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    prepare: ({ title, media, ...toppings }) => {
+      // console.log(title, media, toppings);
+      // 1. filter undefined toppings out
+      const tops = Object.values(toppings).filter(Boolean);
+      // 2. return the preview object for the pizza
+      return {
+        title,
+        media,
+        subtitle: tops.join(', '),
+      };
+    },
+  },
 };
