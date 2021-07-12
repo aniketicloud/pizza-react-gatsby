fe03c69c390616746523e051ee8436654fa797bf
Aniket Pandharabale <aniketicloud@gmail.com>
2021-07-12 23:29:45 +0530

    Custom hook for Client Side Data Fetching

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,5 +1,31 @@
 import React from 'react';
+import useLatestData from '../utils/useLatestData';
+
+function CurrentlySlicing() {
+  return (
+    <div>
+      <p>CurrentlySlicing</p>
+    </div>
+  );
+}
+function HotSlices() {
+  return (
+    <div>
+      <p>HotSlices</p>
+    </div>
+  );
+}
 
 export default function Homepage() {
-  return <p>Hey! I'm home page</p>;
+  const { slicemasters, hotSlices } = useLatestData();
+  return (
+    <div className="center">
+      <h1>The Best Pizza Downtown !</h1>
+      <p>Open from 11am to 11pm Every Single Day</p>
+      <div>
+        <CurrentlySlicing slicemasters={slicemasters} />
+        <HotSlices hotSlices={hotSlices} />
+      </div>
+    </div>
+  );
 }
